561186f0c0b6e5c28ddc3d464a749d7bfdbc4c92
ipar2022 <parnetwork@outlook.com>
2023-02-16 21:25:54 -0600

    Corrected earthquake colorfill.

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -74,7 +74,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
   // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 5) {
-      return "#ea2c2c";
+      return "red";
     }
     if (magnitude > 4) {
       return "#ea822c";
@@ -164,26 +164,27 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     return {
       opacity: 1,
       fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
       color:  "#000000",
-      radius: getRadius(),
+      radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
   }
 
 // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
-function getColor(magnitude){
+//function getColor(magnitude){
 
 
-if (magnitude > 6) {
-  return "#ea822c";
-}
+//if (magnitude > 6) {
+ //return "red";
+//}
 
-if (magnitude > 5) {
-  return "#ee9c00";
-}  
-return "#ea2c2c"}
-//?
+//if (magnitude > 5) {
+// return "#ee9c00";
+//}  
+//return "red"}
+
 // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
 function getRadius(magnitude){
   if (magnitude === 0) {
